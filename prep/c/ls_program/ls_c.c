8bc39481332755d7b0e71fbef8364749e1ad1fca
Paul Haddad <paulh16@gmail.com>
2021-02-06 13:16:52 -0500

    C Completion exersise:
    * Get flags and paths working

--- prep/c/ls_program/ls_c.c
+++ prep/c/ls_program/ls_c.c
@@ -1,11 +1,46 @@
 #include <dirent.h>
 #include <sys/stat.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
+  char* path = ".";
 
-  DIR* directory = opendir(".");
+  bool FLAG_LONG_FORMAT = false;
+  bool FLAG_ALL = false;
+  bool FLAG_SORT_BY_SIZE = false;
+
+  if (argc > 1) {
+    // skip first arg, the program name
+    argv++;
+
+    char * arg;
+    while ((arg = *argv) != NULL) {
+      // Flags
+      if (arg[0] == '-') {
+        if (arg[1] == 'l') {
+          printf("Long format\n");
+          FLAG_LONG_FORMAT = true;
+        }
+        if (arg[1] == 'a') {
+          printf("All flag\n");
+          FLAG_ALL = true;
+        }
+        if (arg[1] == 'S') {
+          printf("Sort by size flag\n");
+          FLAG_SORT_BY_SIZE = true;
+        }
+
+        printf("Flags: %s\n", arg);
+      } else {
+        printf("Arg: %s\n", arg);
+      }
+      argv++;
+    }
+  }
+
+  DIR* directory = opendir(path);
   struct dirent *curdir;
 
   while ((curdir = readdir(directory)) != NULL) {
@@ -24,3 +59,9 @@ int main(int argc, char *argv[]) {
 
   return EXIT_SUCCESS;
 }
+
+// Supported flags:
+// -a: include files beginning with a dot
+// -l: long format
+// -S: Sort by size
+// Add error checking
